07a852db0235f9d2bcf1d54e27f3d111c4e5bcd4
daulet2020 <daulet2020@gmail.com>
2019-11-06 12:43:20 -0600

    New

--- src/test/java/tests/day1/MyFirstSeleniumScript.java
+++ src/test/java/tests/day1/MyFirstSeleniumScript.java
@@ -1,7 +1,7 @@
 package tests.day1;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
+        import io.github.bonigarcia.wdm.WebDriverManager;
+        import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstSeleniumScript {
     public static void main(String[] args) {
